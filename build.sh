c6a1402f2c37a5eaab8a78419a7c0d5f6615cf61
bsanchez <mrsipan@users.noreply.github.com>
2021-10-14 16:24:07 -0400

    Support both centos 7 and 8

--- build.sh
+++ build.sh
@@ -5,7 +5,15 @@ set -o errexit
 set -o xtrace
 
 version=$1
-ver=$(printf "%.3s" $version | sed -e 's/\.//g')
+ver=$(printf "%.4s" $version | sed -e 's/\.//g')
+
+dnf="dnf"
+if [ $(cat /etc/centos-release |  awk '{gsub("[^[:digit:]]+", " "); print $1}') = '7' ]; then
+  dnf="yum"
+fi
+${dnf} -y install gcc make rpm-build
+${dnf} clean all
+test ! -d /build/rpms && mkdir -p /build/rpms
 
 sed "s/PYTHON_VERSION/$version/" clean_python.spec.template | \
   sed "s/PYVER/$ver/" > "clean_python${ver}.spec"
@@ -16,7 +24,7 @@ curl -k -O "https://www.python.org/ftp/python/${version}/Python-${version}.tgz"
 
 spec_path="$(tar -t -f Python-${version}.tgz | grep -e '\.spec$' || true)"
 
-gzip -d Python-${version}.tgz
+gzip -f -d Python-${version}.tgz
 
 if [ -n "$spec_path" ]; then
   printf "spec_path: %s\n" "$spec_path"
@@ -27,10 +35,16 @@ tar -u -f Python-$version.tar "clean_python${ver}.spec"
 
 gzip -c Python-$version.tar > Python-${version}.tgz
 
-rpmbuild -ts --nodeps --define "_sourcedir `pwd`" --define "_srcrpmdir `pwd`/rpms" Python-${version}.tgz
+rpmbuild -ts --nodeps --define "_sourcedir `pwd`" \
+                      --define "_srcrpmdir `pwd`/rpms" \
+                      Python-${version}.tgz
 
 for src_rpm_file in `ls -1 rpms/clean_python*.src.rpm`; do
-  yum-builddep -y $src_rpm_file
+  if [ $dnf = 'yum' ]; then
+    ${dnf}-builddep -y $src_rpm_file
+  else
+    ${dnf} builddep -y $src_rpm_file
+  fi
 done
 
 for src_rpm_file in `ls -1 rpms/clean_python*.src.rpm`; do
